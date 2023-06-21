8a14193c9c716dfd3742f8104dfcaa2905b1712a
WhuzurBuddha <116446513+WhuzurBuddha@users.noreply.github.com>
2023-06-21 13:17:43 +0200

    Update main.cpp

--- netstatGUI/src/main.cpp
+++ netstatGUI/src/main.cpp
@@ -1,17 +1,22 @@
 #include <fstream>
 #include <vector>
-#include "../include/active_connections.h"
-#include "../include/active_conn_without_server.h"
-#include "../include/routing_table.h"
-#include "../include/interfaces.h"
+#include "include/active_connections.h"
+#include "include/active_conn_without_server.h"
+#include "include/routing_table.h"
+#include "include/interfaces.h"
 
 
 void generateContent() {
-    std::vector<std::string> functions = {"routing_table()", "interfaces()", "active_connections()", "active_conn_without_server()"};
+    std::string  routing = routing_table();
+    std::string  interface = interfaces();
+    std::string active_connection = active_connections();
+    std::string active_conn_server = active_conn_without_server();
+
+    std::vector<std::string>  functions = {routing, interface, active_connection, active_conn_server};
     std::vector<std::string> output_files = {"outrouting.txt", "interout.txt", "acs.txt", "acws.txt"};
     for(int i = 0; i < functions.size(); ++i){
         std::string output = functions[i];
-        std::ofstream outputFile("/NETstetGUI/output/" + output_files[i], std::ofstream::trunc);
+        std::ofstream outputFile("/NetStatGUI/GUI/output/" + output_files[i], std::ofstream::trunc);
         if (outputFile.is_open()) {
             outputFile << output << std::endl;
             outputFile.close();
@@ -25,5 +30,4 @@ int main() {
     generateContent();
     return 0;
 }
-
 
