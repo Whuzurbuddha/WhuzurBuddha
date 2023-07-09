0c0e106153320b16d16ca05b853412366fd693b6
WhuzurBuddha <116446513+WhuzurBuddha@users.noreply.github.com>
2023-07-09 11:34:53 +0200

    Create gui.py

---
+++ hardware_gui/gui.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+
+######################################
+##                                  ##
+##                                  ##
+## Written by: Qt Alexander Päplow  ##
+##                                  ##
+##                                  ##
+######################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+from show_audio_info import show_audio_info
+from show_gpu_info import show_gpu_info
+
+class Ui_Hardwarecomponents(object):
+    def setupUi(self, Hardwarecomponents):
+        if not Hardwarecomponents.objectName():
+            Hardwarecomponents.setObjectName(u"Hardwarecomponents")
+        Hardwarecomponents.resize(1342, 870)
+        Hardwarecomponents.setAutoFillBackground(False)
+        self.audioButton = QPushButton(Hardwarecomponents)
+        self.audioButton.setObjectName(u"audioButton")
+        self.audioButton.setGeometry(QRect(0, 10, 127, 44))
+        self.audioButton.setCheckable(False)
+        self.cpuButton = QPushButton(Hardwarecomponents)
+        self.cpuButton.setObjectName(u"cpuButton")
+        self.cpuButton.setGeometry(QRect(0, 60, 127, 44))
+        self.gpuButton = QPushButton(Hardwarecomponents)
+        self.gpuButton.setObjectName(u"gpuButton")
+        self.gpuButton.setGeometry(QRect(0, 110, 127, 44))
+        self.textBrowser = QTextBrowser(Hardwarecomponents)
+        self.textBrowser.setObjectName(u"textBrowser")
+        self.textBrowser.setGeometry(QRect(140, 10, 1191, 851))
+        self.textBrowser.setSizeAdjustPolicy(QAbstractScrollArea.AdjustToContents)
+        self.textBrowser.setAutoFormatting(QTextEdit.AutoAll)
+        self.storageButton = QPushButton(Hardwarecomponents)
+        self.storageButton.setObjectName(u"storageButton")
+        self.storageButton.setGeometry(QRect(0, 160, 127, 44))
+
+        self.retranslateUi(Hardwarecomponents)
+
+        QMetaObject.connectSlotsByName(Hardwarecomponents)
+        self.audioButton.clicked.connect(self.show_audio_info)
+        self.cpuButton.clicked.connect(self.show_cpu_info)
+        self.gpuButton.clicked.connect(self.show_gpu_info)
+        self.storageButton.clicked.connect(self.show_storage_info)
+
+    def retranslateUi(self, Hardwarecomponents):
+        Hardwarecomponents.setWindowTitle(QCoreApplication.translate("Hardwarecomponents", u"Hardware components", None))
+        self.audioButton.setText(QCoreApplication.translate("Hardwarecomponents", u"Audio", None))
+        self.cpuButton.setText(QCoreApplication.translate("Hardwarecomponents", u"CPU", None))
+        self.gpuButton.setText(QCoreApplication.translate("Hardwarecomponents", u"GPU", None))
+        self.storageButton.setText(QCoreApplication.translate("Hardwarecomponents", u"Storage", None))
+
+    def show_audio_info(self):
+        audio_info = str(show_audio_info())
+        self.textBrowser.setText(audio_info)
+
+    def show_cpu_info(self):
+        self.textBrowser.setText("CPU information goes here.")
+
+    def show_gpu_info(self):
+        gpu_info = str(show_gpu_info())
+        self.textBrowser.setText(gpu_info)
+
+    def show_storage_info(self):
+        self.textBrowser.setText("Storage information goes here.")
+
+
+if __name__ == "__main__":
+    import sys
+
+    app = QApplication(sys.argv)
+    Hardwarecomponents = QMainWindow()
+    ui = Ui_Hardwarecomponents()
+    ui.setupUi(Hardwarecomponents)
+    Hardwarecomponents.show()
+    sys.exit(app.exec_())
