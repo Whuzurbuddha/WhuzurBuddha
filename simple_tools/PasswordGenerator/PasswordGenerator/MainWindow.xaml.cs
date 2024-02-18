a48bd99bef793c50ac8bc5bda4b1f5f37ea4327e
Alexander Päplow <116446513+Whuzurbuddha@users.noreply.github.com>
2024-02-18 22:05:12 +0100

    Add files via upload

---
+++ simple_tools/PasswordGenerator/PasswordGenerator/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Security.Cryptography;
+using System.Windows;
+
+namespace PasswordGenerator;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    [Obsolete("Obsolete")]
+    private void GeneratePassword(object sender, RoutedEventArgs gp)
+    {
+        var length = (int)Slider.Value;
+        Console.Write(length);
+        const string validChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;:'\",.<>?";
+        var password = new char[length];
+
+        using (var rng = new RNGCryptoServiceProvider())
+        {
+            var validCharCount = validChars.Length;
+            var randomNumber = new byte[1];
+
+            for (var i = 0; i < length; i++)
+            {
+                do
+                {
+                    rng.GetBytes(randomNumber);
+                } while (randomNumber[0] >= validCharCount * (Byte.MaxValue / validCharCount));
+
+                var randomCharIndex = randomNumber[0] % validCharCount;
+
+                password[i] = validChars[randomCharIndex];
+            }
+        }
+
+        var newPassword = new string(password);
+        NewPassword.Text = newPassword;
+        Clipboard.SetText(newPassword);
+        Info.Visibility = Visibility.Visible;
+    }
+}
